95de61d232e4c4baafb51c5f7623aad4ea13d59d
ahmadrazza2001 <126485111+ahmadrazza2001@users.noreply.github.com>
2023-08-16 01:52:33 +0500

    keys commit

--- server/middlwares/authMiddleware.js
+++ server/middlwares/authMiddleware.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/keys");
 module.exports = (req, res, next) => {
   try {
     // get token from header
     const token = req.header("authorization").split(" ")[1];
-    const decryptedToken = jwt.verify(token, process.env.jwt_secret);
+    const decryptedToken = jwt.verify(token, jwt_secret);
     req.body.userId = decryptedToken.userId;
     next();
   } catch (error) {
